dfc7f87cf3a8a7a0ead698011326d3e8971ee05e
ThomasTheDebugEngine <thomaskilinc09@gmail.com>
2020-02-12 18:15:19 +0200

    modified request to get resources by passing in ID

--- src/API/StacksAPI.ts
+++ src/API/StacksAPI.ts
@@ -1,30 +1,36 @@
 import axios, {AxiosResponse} from "axios";
 
-/*
-function QueryBuilder(OptionsArr: string[]){ //TODO for later automation
-    let URL: string = "";
-    let UrlBase = "http://"
-}
-*/
 
-async function SendRequest(){
+function QueryBuilder(InputID: number): string{ //TODO for later automation
+    //let UrlParams: string = ""; //for future use
+    let UrlBase :string= "http://resourceshare.herokuapp.com/api/stacks"
+    const FinalUrl :string = UrlBase + "/" + InputID;
+    //console.log(FinalUrl)
+    return FinalUrl;
+}
+
+
+async function SendRequest(InputID: number){
     //Response.data.stacks is an array
-    let URL: string = "http://resourceshare.herokuapp.com/api/stacks";
+    const Url: string = QueryBuilder(InputID);
 
     try {
-        var Response: AxiosResponse = await axios.get(URL);    
+        var Response: AxiosResponse = await axios.get(Url);    
     }
     catch (ex) {
         console.error("ERROR SendRequest(): " + ex)
     }
-    //console.log(Response.data.stacks); //for debug
+    console.log(Response.data.resources); //for debug
     return Response;
     
 }
 
-//SendRequest(); //for debug
+SendRequest(1); //for debug
 
 /*
 to use the data remove console.log() and pass data as parameter
 SendRequest().then(function(Response){console.log(Response.data.stacks)});
 */
+
+
+//SendRequest(1).then(function(Response){console.log(Response.data.stacks)});
